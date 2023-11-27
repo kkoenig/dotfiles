26e17d4a9fe0c0528bb35fc974e92d82c2115769
Killian Koenig <killiankoenig@gmail.com>
2023-11-27 11:26:09 -0800

    Add fzf theme and history config

--- home.nix
+++ home.nix
@@ -62,6 +62,29 @@
     enableBashIntegration = true;
     enableFishIntegration = false;
     enableZshIntegration = false;
+    defaultOptions = [
+      "--margin 5,10%"
+    ];
+    historyWidgetOptions = [
+      "--height 100%"
+      "--preview 'echo {}' --preview-window up:3:hidden:wrap"
+      "--bind 'ctrl-w:toggle-preview'"
+      "--bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'"
+    ];
+    colors = {
+      fg = "#f8f8f2";
+      bg = "#282a36";
+      hl = "#bd93f9";
+      "fg+" = "#f8f8f2";
+      "bg+" = "#44475a";
+      "hl+" = "#bd93f9";
+      info = "#ffb86c";
+      prompt = "#50fa7b";
+      pointer = "#ff79c6";
+      marker = "#ff79c6";
+      spinner = "#ffb86c";
+      header = "#6272a4";
+    };
   };
 
   programs.alacritty = {
