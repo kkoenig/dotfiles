ef8e756781f9a4ab77bfc88887b241b627eb50e8
Killian Koenig <killiankoenig@gmail.com>
2023-11-27 23:01:35 -0800

    Fix tmux color scheme

--- home/tmux.nix
+++ home/tmux.nix
@@ -1,5 +1,20 @@
-{
+{ pkgs, ... }: {
   programs.tmux = {
     enable = true;
+    prefix = "C-a";
+    extraConfig =
+      "set-option -g terminal-overrides \",alacritty:RGB\""
+    ;
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.dracula;
+        extraConfig = "
+	  set-option -g @dracula-plugins \" \"
+	  set-option -g @dracula-show-powerline true
+	  set-option -g @dracula-show-flags false
+	  set-option -g @dracula-show-left-icon \"k\"
+	";
+      }
+    ];
   };
 }
