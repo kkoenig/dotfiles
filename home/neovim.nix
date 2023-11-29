df162f36ea40a700d3d729b45849318da3196752
Killian Koenig <killiankoenig@gmail.com>
2023-11-28 17:37:04 -0800

    Update vim settings

--- home/neovim.nix
+++ home/neovim.nix
@@ -9,12 +9,23 @@
       lualine-nvim
       fzf-lua
     ];
+    extraConfig = /* vimscript */ ''
+      set expandtab       " use spaces instead of tabs in insert mode
+      set number          " show line numbers
+      set shiftwidth=4    " number of spaces to use for each autoindent
+      set ignorecase      " ignore case when matching
+      set smartcase       " override ignorecase setting if search contains uppercase
+      set softtabstop=-1  " use shiftwidth to insert spaces when <TAB> is pressed
+      set tabstop=4       " number of spaces that a tab in a file counts for
+      set splitright      " open new vertical panes right
+      set list listchars=tab:>-\ ,eol:\ ,trail:~,extends:>,precedes:< " display trailing whitespace
+      set clipboard+=unnamedplus
+
+      inoremap jj <ESC>
+    '';
     extraLuaConfig = /* lua */ ''
       vim.o.termguicolors = true
       vim.cmd('colorscheme dracula')
-
-      vim.o.expandtab = true
-      vim.o.number = true
 
       require('lualine').setup()
       require("fzf-lua").setup({ 
