2a27c6f78d6ba8f89bb796ca1f79ec7a85a6eeb6
Killian Koenig <killiankoenig@gmail.com>
2024-04-23 16:23:08 -0700

    Update and add a few more lang servers

--- home/neovim/init.lua
+++ home/neovim/init.lua
@@ -29,7 +29,18 @@ lsp.on_attach(function(_, bufnr)
 end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-require('lspconfig').gopls.setup {}
+local lspconfig = require("lspconfig")
+lspconfig.gopls.setup({
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+        },
+    },
+})
 require('lspconfig').clangd.setup(
     {
         cmd = {
